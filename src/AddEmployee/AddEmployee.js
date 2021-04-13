5ca3ceda510a50628d02cab37296532239e4cddd
Nandhagopal maddy <>
2021-04-13 04:36:09 +0000

    changes

--- src/AddEmployee/AddEmployee.js
+++ src/AddEmployee/AddEmployee.js
@@ -3,11 +3,10 @@ import { useEffect, useState }  from 'react';
 import axios from 'axios';
 import useToken from '../useToken'
 
-function getLastId(){
-var res= axios.get('https://employeeBackend.nandhagopalmadd.repl.co/GetLastId')
-return res.last
-}
 
+function handleSubmit(state){
+console.log(state)
+}
 
 export default function AddEmployee() {
   var token= sessionStorage.getItem('token')
@@ -25,6 +24,29 @@ export default function AddEmployee() {
     user:"",
     pass:""
   })
+  useEffect(()=>{
+    getLastId()
+  },[])
+  function getLastId(){
+axios.get('https://employeeBackend.nandhagopalmadd.repl.co/GetLastId').then((res)=>{
+    //console.log(res.data)
+    let t=res.data.last+1
+    var temp=state  
+        temp.EmpId=t
+        setState(temp)
+})
+
+}
+  function generateMail(){
+    let id=state.EmpName.replace(' ','')
+    id=id.length>10?id.slice(0,10):id
+    let m=id+'.'+id[0]+'@EmpMgmt.com'
+    var temp=state  
+        temp.EmpMail=m       
+        setState(temp)
+  
+}
+  
   
   if(token==='"Admin"')
   {
@@ -33,20 +55,35 @@ export default function AddEmployee() {
       <h2>Add Employees</h2>
       <form>
       <div className='form-group row'>
-        <p className='col-md-2 offset-md-2'>Username</p>
+        <p className='col-md-2 offset-md-2'>Employee ID</p>
+        <input className="form-control col-md-5" type="text" disabled value={state.EmpId}/>
+      <br/>      
+      </div>
+      <div className='form-group row'>
+        <p className='col-md-2 offset-md-2'>Name</p>
         <input className={state.EmpName?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" onChange={e=>{
-        var temp={EmpName:e.target.value}  
+        var temp=state  
+        temp.EmpName=e.target.value        
         setState(temp)
+        
         }
         }
         />
       <br/>
       {state.EmpName?.length<=0?<div className='offset-md-4 col-md-4 text-danger'>Employee name cannot be empty</div>:null}
       </div>
+     <div className='form-group row'>
+        <p className='col-md-2 offset-md-2'>Employee Mail</p>
+        <input className="form-control col-md-5" type="text" disabled value={state.EmpMail}/>
+      <br/>
+      {state.EmpName?.length>3?<div onClick={generateMail} className='offset-sm-1 btn btn-primary'>Generate MailID</div>:null}
+      </div>
+
       <div className='form-group row'>
         <p className='col-md-2 offset-md-2'>Designation</p>
-        <input className={state.EmpDesignation?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" onChange={e=>{
-        var temp={EmpDesignation:e.target.value}  
+        <input className={state.EmpDesignation?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" onChange={e=>{        
+        var temp=state  
+        temp.EmpDesignation=e.target.value        
         setState(temp)
         }
         }
@@ -56,8 +93,9 @@ export default function AddEmployee() {
       </div>
       <div className='form-group row'>
         <p className='col-md-2 offset-md-2'>Contact No</p>
-        <input className={state.EmpContactNumber?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" onChange={e=>{
-        var temp={EmpContactNumber:e.target.value}  
+        <input className={state.EmpContactNumber?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" onChange={e=>{          
+        var temp=state  
+        temp.EmpContactNumber=e.target.value        
         setState(temp)
         }
         }
@@ -67,19 +105,22 @@ export default function AddEmployee() {
       </div>
       <div className='form-group row'>
         <p className='col-md-2 offset-md-2'>Certifications</p>
-        <input className={state.EmployeeCerts?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" onChange={e=>{
-        var temp={EmployeeCerts:e.target.value}  
-        setState(temp)
+        <input className={state.EmployeeCerts?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"} type="text" placeholder='ex:Angular,Nodejs,MongoDB' onChange={e=>{  
+        var temp=state  
+        temp.EmployeeCerts=e.target.value        
+        setState(temp)        
         }
         }
         />
       <br/>
+      <small>Enter your certifications , seperated</small>
       {state.EmployeeCerts?.length<=0?<div className='offset-md-4 col-md-4 text-danger'>Employee Contact cannot be empty</div>:null}
       </div>
       <div className='form-group row'>
         <p className='col-md-2 offset-md-2'>Address</p>
-        <textarea className={state.EmpAddress?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"}  onChange={e=>{
-        var temp={EmpAddress:e.target.value}  
+        <textarea className={state.EmpAddress?.length<=0?"form-control is-invalid col-md-5":"form-control col-md-5"}  onChange={e=>{         
+        var temp=state  
+        temp.EmpAddress=e.target.value        
         setState(temp)
         }
         }
@@ -89,7 +130,8 @@ export default function AddEmployee() {
       </div>
       
       </form>
-      <div className='btn btn-primary' onClick={()=>window.history.back()}>Go back</div>
+      <div class='btn btn-primary offset-sm-2' onClick={()=>handleSubmit(state)}>Submit</div>
+      <div className='btn btn-danger offset-md-6' onClick={()=>window.history.back()}>Go back</div>
       </div>
     );
   }else{
